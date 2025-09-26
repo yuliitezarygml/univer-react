0aa955bde3998ee63ccf0c1c2d962fec39cefe3f
yuliitezarygml <yuliitezary@hotmail.com>
2025-09-26 11:36:17 +0300

    connecta table and data.json

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./App.css";
+import data from "./data.json"; 
+
+function App() {
+  return (
+    <div className="App">
+       {Object.entries(data).map(([key, items]) => (
+            <table border="1" key={key}>
+        
+        <thead>
+          <tr>
+            <td colSpan="5" style={{backgroundColor: '#f2f2f2', textAlign: 'center', fontWeight: 'bold' }}>
+                Ziua lunii {key}
+              </td>
+          </tr>
+          <tr>
+            <th>ora</th>
+            <th>lectia</th>
+            <th>Sala</th>
+            <th>numele</th>
+            <th>P-S-L</th>
+          </tr>
+        </thead>
+        <tbody>
+              {items.map((item) => (
+                <tr key={item.id} {...console.log(item)}>
+                <td>{item.ora}</td>
+                <td>{item.lectia}</td>
+                <td>{item.sala}</td>
+                <td>{item.numele}</td>
+                <td>{item.psl}</td>
+              </tr>
+              ))} 
+        </tbody>
+      </table>
+          ))}
+      
+          
+       
+    </div>
+  );
+}
+
+export default App;
