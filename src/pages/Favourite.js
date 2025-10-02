78b742f81853fcb284529ada1ddb6c34f386b1dd
yuliitezarygml <yuliitezary@hotmail.com>
2025-10-02 09:55:03 +0300

    add

---
+++ src/pages/Favourite.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Favourite() {
+    return (
+        <div style={{ padding: "2rem", textAlign: "center" }}>
+            <h1>Your Favourites</h1>
+            <p>Here you can find all your favourite fashion items.</p>
+        </div>
+    );
+}
+
+export default Favourite;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PromoBanner from "../component/PromoBanner";
+
+function Home() {
+    return (
+        <div>
+            <PromoBanner />
+        </div>
+    );
+}
+
+export default Home;
